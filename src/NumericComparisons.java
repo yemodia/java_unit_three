f52134e7f6f57101f33dc03df213b67eb2ca7e16
diaye <yerim-oumar.dia@ssfs.org>
2019-10-22 13:33:01 -0400

    Finished part II of the boolean expression exercises

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -22,7 +22,15 @@ public class NumericComparisons {
     private static boolean isNonNegative(int num1){
         return num1 >= 0;
     }
+
+    private  static boolean isFactor(int num1, int num2){ return num2 % num1 == 0;
+    }
+
+    private static boolean isMultiple(int num1, int num2){ return num1 % num2 == 0;
+    }
     public static void main(String[] args){
+
+
 
         System.out.println(isZero(3));
         System.out.println(isOdd(5));
@@ -30,6 +38,9 @@ public class NumericComparisons {
         System.out.println(isNegative(-1));
         System.out.println(isNonNegative(-8));
         System.out.println(isNonPositive(5));
+        System.out.println(isFactor(2, 10));
+        System.out.println(isMultiple(5, 3));
+
 
 
     }
