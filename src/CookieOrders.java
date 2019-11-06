8988be80a7e2d57fb0e101daa114f29cfdc48de8
diaye <yerim-oumar.dia@ssfs.org>
2019-11-06 08:58:47 -0500

    finished cookie order program

--- src/CookieOrders.java
+++ src/CookieOrders.java
@@ -1,28 +1,43 @@
-//import java.util.Scanner;
-//
-//public class CookieOrders {
-//
-//    private static String totalCost(int orderNum){
-//        if (orderNum>= 1 || orderNum <= 4)
-//            System.out.println("1-4 boxes:				6.95 per box");
-//        else if (orderNum <= 9)
-//            System.out.println("5-9 boxes:\t\t\t\t5.95 per box\n");
-//        else if (orderNum <= 15)
-//            System.out.println("10-15 boxes:\t\t\t\t5.50 per box");
-//        else
-//            System.out.println("16 or more boxes:\t\t\t4.95 per box");
-//        return orderNum;
-//    }
-//    public static void main(String[] args){
-//
-//            System.out.println("1-4 boxes:				6.95 per box");
-//            System.out.println("5-9 boxes:\t\t\t\t5.95 per box\n");
-//            System.out.println("10-15 boxes:\t\t\t\t5.50 per box");
-//            System.out.println("16 or more boxes:\t\t\t4.95 per box");
-//
-//            System.out.print("Enter the amount of boxes you wish to purchase.: ");
-//        Scanner scan = new Scanner(System.in);
-//
-//
-//    }
-//}
+
+import java.util.Scanner;
+
+public class CookieOrders {
+
+    private static String totalCost(int orderNum){
+        double boxPrice;
+        if (orderNum>= 1 && orderNum <= 4){
+            boxPrice = 6.95;
+            return  "You bought " + orderNum +" box of cookies at $" + boxPrice +" per box. Your total bill is $ "+orderNum*boxPrice;
+        }
+        else if (orderNum >= 5 && orderNum <= 9){
+            boxPrice = 5.95;
+            return  "You bought " + orderNum +" box of cookies at $" + boxPrice +" per box. Your total bill is $ "+orderNum*boxPrice;
+        }
+        else if (orderNum >= 10 && orderNum <= 15){
+            boxPrice = 5.50;
+            return  "You bought " + orderNum +" box of cookies at $" + boxPrice +" per box. Your total bill is $ "+orderNum*boxPrice;
+        }
+        else if (orderNum > 16){
+            boxPrice = 4.95;
+            return  "You bought " + orderNum +" box of cookies at $" + boxPrice +" per box. Your total bill is $ "+orderNum*boxPrice;
+        }
+        else
+            return "Your input is invalid";
+
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+            System.out.println("1-4 boxes:				6.95 per box");
+            System.out.println("5-9 boxes:\t\t\t\t5.95 per box\n");
+            System.out.println("10-15 boxes:\t\t\t\t5.50 per box");
+            System.out.println("16 or more boxes:\t\t\t4.95 per box");
+
+            System.out.print("Enter the amount of boxes you wish to purchase: ");
+            int orderNum = scan.nextInt();
+
+            System.out.println(totalCost(orderNum));
+
+
+    }
+}
