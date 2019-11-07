008f0e584bd582db5fd6a0a1e375acbbec3e437d
diaye <yerim-oumar.dia@ssfs.org>
2019-11-07 10:25:14 -0500

    Finished righttriangle program

---
+++ src/RightTriangle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class RightTriangle {
+    private static Boolean isTriangle(int a, int b, int c) {
+        return ((a + b > c) && (a + c > b) && (b + c > a));
+    }
+
+    private static Boolean isRight(int a, int b, int c){
+
+        double pythagorean = Math.pow(a, 2) + Math.pow(b, 2);
+
+
+        if (pythagorean == Math.pow(c, 2))
+            return true;
+        else return false;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter integer sides of a triangle");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.print(isRight(a,b,c));
+        System.out.print(isTriangle(a,b,c));
+    }
+}
+
+
